24dd574502115dbefb9b1db822440d80040d587d
Rishabh Singh <dailylifesking@gmail.com>
2020-11-11 13:23:23 +0530

    Update time.js

--- f/time.js
+++ f/time.js
@@ -9,12 +9,11 @@ if(time<585)celld=1
 else if(time<645)celld=2
 else if(time<705)celld=3
 else if(time<765)celld=4
-else if(time<855)celld=5
-else if(time<915)celld=6
+else if(time<870)celld=5
+else if(time<930)celld=6
 else{celld=1;cellr=cellr+1;if(cellr>4){cellr=1}}
 
 if(cellr==0){cellr=1;celld=1}
-if(cellr==2 && celld==6){cellr=3;celld=1}
 if(cellr==3 && celld==6){cellr=4;celld=1}
 if(cellr==3 && celld==5){cellr=4;celld=1}
 
@@ -23,5 +22,21 @@ document.head.append(newStyles);
 newStyles.innerHTML = "tr:nth-child("+(cellr+1)+") td:nth-child("+(celld+1)+"){background-color: #ffff00}";
 
 var t=document.getElementById("t").children[0].children[cellr].children[celld].children[1]
-for(i=0; i<4; i++) {t.appendChild(document.createElement('span'))}
-document.getElementById("t").children[0].children[cellr-1].children[celld].scrollIntoView(true)
+var t2=document.getElementById("t2").children[0].children[cellr].children[celld].children[1]
+for(i=0; i<4; i++) {
+	t.appendChild(document.createElement('span'));
+	t2.appendChild(document.createElement('span'))}
+
+document.getElementById("t").style.display = "table";
+document.getElementById("t2").style.display = "none";
+function CS33(){
+	document.getElementById("t2").style.display = "none";
+	document.getElementById("t").style.display = "table";
+	localStorage.setItem("c",'3');
+	document.getElementById("t").children[0].children[1].children[celld].scrollIntoView(true)}
+function CS34(){
+	document.getElementById("t").style.display = "none";
+	document.getElementById("t2").style.display = "table";
+	localStorage.setItem("c",'4');
+	document.getElementById("t2").children[0].children[1].children[celld].scrollIntoView(true)}
+if(localStorage.getItem("c")=="4"){CS34()}
