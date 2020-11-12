84cb959a06b1610b859b94556c59f00124e2c3b6
Rishabh Singh <dailylifesking@gmail.com>
2020-11-12 13:48:50 +0530

    Update time.js

--- f/time.js
+++ f/time.js
@@ -8,7 +8,7 @@ else if(time<705)celld=3
 else if(time<765)celld=4
 else if(time<870)celld=5
 else if(time<930)celld=6
-else{celld=1;cellr=cellr+1;if(cellr>4)cellr=1;}
+else{celld=1;cellr=cellr+1;if(cellr>5)cellr=1;}
 
 if(cellr==0){cellr=1;celld=1}
 if(cellr==3 && (celld==5 || celld==6)){cellr=4;celld=1}
