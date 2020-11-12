7d49d975580e34274680e2d589a33e13c1da565a
Rishabh Singh <dailylifesking@gmail.com>
2020-11-12 12:54:24 +0530

    Update time.js

--- f/time.js
+++ f/time.js
@@ -1,7 +1,5 @@
-var myDate = new Date(); 
-var minutes = myDate.getMinutes();
-var hours = myDate.getHours();
-var time = hours*60 + minutes;
+var myDate = new Date();
+var time = myDate.getHours()*60 + myDate.getMinutes;
 var celld=1;
 var cellr = myDate.getDay();
 
@@ -10,11 +8,10 @@ else if(time<705)celld=3
 else if(time<765)celld=4
 else if(time<870)celld=5
 else if(time<930)celld=6
-else{celld=1;cellr=cellr+1;if(cellr>4){cellr=1}}
+else{celld=1;if(cellr>3)cellr=1;}
 
 if(cellr==0){cellr=1;celld=1}
-if(cellr==3 && celld==6){cellr=4;celld=1}
-if(cellr==3 && celld==5){cellr=4;celld=1}
+if(cellr==3 && (celld==5 || celld==6)){cellr=4;celld=1}
 
 var newStyles = document.createElement('style')
 document.head.append(newStyles);
@@ -22,9 +19,7 @@ newStyles.innerHTML = "tr:nth-child("+(cellr+1)+") td:nth-child("+(celld+1)+"){b
 
 var t=document.getElementById("t").children[0].children[cellr].children[celld].children[1]
 var t2=document.getElementById("t2").children[0].children[cellr].children[celld].children[1]
-for(i=0; i<4; i++) {
-	t.appendChild(document.createElement('span'));
-	t2.appendChild(document.createElement('span'))}
+for(i=0; i<4; i++) {t.appendChild(document.createElement('span'));t2.appendChild(document.createElement('span'))}
 
 function CS33(){
 	document.getElementById("t2").style.display = "none";
@@ -36,6 +31,5 @@ function CS34(){
 	document.getElementById("t2").style.display = "table";
 	localStorage.setItem("c",'4');
 	document.getElementById("t2").children[0].children[1].children[celld].scrollIntoView(true)}
-
 if(localStorage.getItem("c")=="4")CS34();
 else CS33();
