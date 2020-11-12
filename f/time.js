15db40957fe13ed9f6c7504e3df8b35e8675e136
Rishabh Singh <dailylifesking@gmail.com>
2020-11-12 14:44:36 +0530

    Update time.js

--- f/time.js
+++ f/time.js
@@ -1,5 +1,5 @@
 var myDate = new Date();
-var time = (myDate.getHours())*60 + myDate.getMinutes;
+var time = myDate.getHours()*60 + myDate.getMinutes();
 var celld=1;
 var cellr = myDate.getDay();
 
